18bea120e759701e24907817d7cfb025669c1fec
Svetlana <svetlana.tsibenova@gmail.com>
2022-12-21 15:30:23 +0300

    updated app.js

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ require('@babel/register');
 const React = require('react');
 const ReactDomServer = require('react-dom/server');
 const express = require('express');
+const path = require('path');
 
 const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log('Server started'));
