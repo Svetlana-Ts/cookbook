5094b811aa845d4fa241156c88a7bfda5374c608
Anastasiya <iinokayo@mail.ru>
2022-12-21 15:43:53 +0300

    Updated app.js

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 require('dotenv').config();
 require('@babel/register');
+
 const React = require('react');
 const ReactDomServer = require('react-dom/server');
+
 const express = require('express');
+const logger = require('morgan');
 const path = require('path');
 
 const app = express();
@@ -11,4 +14,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log('Server started'));
+
+app
+  .listen(PORT)
+  .on('error', (error) => {
+    console.log('Ошибка при подключении сервера: ', error.message);
+  })
+  .on('listening', () => {
+    console.log(`Серевер успешно подключен к порту ${PORT}`);
+  });
