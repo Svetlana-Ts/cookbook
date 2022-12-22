cab3f7d88eca2f3eea2da05329680e38d718f941
Nickolay-Klimentov <moody.klmn@icloud.com>
2022-12-22 15:39:14 +0300

    views: add main list 1/2

--- views/Main.jsx
+++ views/Main.jsx
@@ -11,7 +11,21 @@ module.exports = function Main({ isAuth }) {
         <div>MENU</div>
         <Menu isAuth={isAuth} />
       </header>
-      <h1>MAIN</h1>
+      <body>
+        <h1>MAIN PAGE</h1>
+        <div className="card">
+          <div className="card-photo">
+            <h1>Photo</h1>
+            <button type="submit" className="card-btn-like">Like</button>
+            <img src="" className="card-photo-img" alt="" />
+          </div>
+          <div className="card-body">
+            <a className="card-body-name" href="/recipe">Card title</a>
+            <div className="card-body-ingr-count">Ingridient count</div>
+            <div className="card-body-cook-time">Cooking time</div>
+          </div>
+        </div>
+      </body>
     </Layout>
   );
 };
