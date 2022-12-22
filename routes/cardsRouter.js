08b59a9a49743e34be9ae03b9f5b9e75a8b22411
Anastasiya <iinokayo@mail.ru>
2022-12-22 17:01:36 +0300

    Fixed routes

--- routes/cardsRouter.js
+++ routes/cardsRouter.js
@@ -1,16 +1,23 @@
 const cardsRouter = require('express').Router();
-const Main = require('../views/Main');
 const { Card: CardModel } = require('../db/models');
+const Main = require('../views/Main');
 const Recipe = require('../views/Recipe');
+const Error = require('../views/Error');
 
 cardsRouter.get('/', async (req, res) => {
-  const cards = await CardModel.findAll();
-  let isAuth = false;
-  if (req.session.userId) {
-    isAuth = true;
+  try {
+    const cards = await CardModel.findAll();
+    let isAuth = false;
+    if (req.session.userId) {
+      isAuth = true;
+      res.renderComponent(Main, { isAuth, cards });
+    }
     res.renderComponent(Main, { isAuth, cards });
+  } catch (error) {
+    res.status(500);
+    res.renderComponent(Error, { error });
   }
-  res.renderComponent(Main, { isAuth, cards });
+  res.end();
 });
 
 cardsRouter.get('/:id', (req, res) => {
