887ebe2344203f95df8809a22faf37e23a50f15f
Anastasiya <iinokayo@mail.ru>
2022-12-22 10:15:27 +0300

    Changed login

--- views/Login.jsx
+++ views/Login.jsx
@@ -5,7 +5,7 @@ module.exports = function Login() {
   return (
     <Layout>
       <h1>Вход</h1>
-      <form method="post" action="/auth/register">
+      <form method="post" action="/auth/login">
         <label htmlFor="emailInput">Почта</label>
         <input type="email" id="emailInput" name="email" />
 
