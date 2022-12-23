91ff6eb8521c33049f69e816686405579bd779e1
Anastasiya <iinokayo@mail.ru>
2022-12-23 13:39:29 +0300

    Sort done

--- db/migrations/20221221123917-create-card.js
+++ db/migrations/20221221123917-create-card.js
@@ -29,7 +29,7 @@ module.exports = {
         allowNull: false,
       },
       time: {
-        type: Sequelize.TEXT,
+        type: Sequelize.INTEGER,
         allowNull: false,
       },
       createdAt: {
--- views/CardList.jsx
+++ views/CardList.jsx
@@ -5,11 +5,32 @@ const Main = require('./Main');
 module.exports = function CardList({ isAuth, cards, userLogin }) {
   return (
     <Main userLogin={userLogin} isAuth={isAuth}>
-      <div>
-        <p>Сортировать по:</p>
-        <a href="/cards/?order=time&sort=desc">времени приготовления</a>
-        <a href="/cards/?order=ingredients&sort=desc">ингредиентам</a>
-      </div>
+      <ul>
+        <li>
+          времени приготовления
+          <ul>
+            <li>
+              <a href="/cards/?order=time&sort=ASC">по возрастанию</a>
+            </li>
+            <li>
+              <a href="/cards/?order=time&sort=DESC">по убыванию</a>
+            </li>
+          </ul>
+        </li>
+        <li>
+          ингредиентам
+          <ul>
+            <li>
+              <a href="/cards/?order=ingredientsCount&sort=ASC">
+                по возрастанию
+              </a>
+            </li>
+            <li>
+              <a href="/cards/?order=ingredientsCount&sort=DESC">по убыванию</a>
+            </li>
+          </ul>
+        </li>
+      </ul>
       <ul>
         {cards.map((card) => (
           <Card key={card.id} isAuth={isAuth} card={card} />
