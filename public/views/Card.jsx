8a943da1ba772de06ddd438884392fc6cc24e76a
Svetlana <svetlana.tsibenova@gmail.com>
2022-12-27 23:06:28 +0300

    changed card's styles

--- public/views/Card.jsx
+++ public/views/Card.jsx
@@ -3,6 +3,7 @@ const React = require('react');
 function Card({ isAuth, card }) {
   return (
     <li className="card">
+
       <div className="card-photo">
         {isAuth && (
           <a href={`/favourites/${card.id}`} className="card-btn-like">
@@ -17,14 +18,27 @@ function Card({ isAuth, card }) {
           alt=""
         />
       </div>
-      <div className="card-body">
-        <p />
-        <b>{card.title}</b>
-        <div className="card-body-cook-time">{`Cooking time: ${card.time} minutes`}</div>
-        <div className="card-body-ingr-count">{`Ingredients count: ${card.ingredients.split('|').length} products`}</div>
-        <a className="card-body-name" href={`/cards/${card.id}`}>View the recipe</a>
+
+      <div className="card-body"> 
+        <div className="card-body-name">
+          <a href={`/cards/${card.id}`}>{card.title}</a>
+        </div>
+
+        <table className="card-table">
+          <tr>
+            <th>Cooking time:</th>
+            <th>Ingredients count:</th>
+          </tr>
+
+          <tr>
+            <td>{`${card.time} minutes`}</td>
+            <td>{`${card.ingredients.split('|').length} products`}</td>
+          </tr>
+          
+        </table>
+        
+        {/* <a className="card-body-name" href={`/cards/${card.id}`}>View the recipe</a> */}
       </div>
-      <p />
     </li>
   );
 }
--- public/views/CardList.jsx
+++ public/views/CardList.jsx
@@ -5,30 +5,29 @@ const Main = require('./Main');
 module.exports = function CardList({ isAuth, cards, userLogin, baseUrl }) {
   return (
     <Main userLogin={userLogin} isAuth={isAuth}>
-      <div className="sort-nav">
-        <div id="card-list">Сортировка по:</div>
-        <div className="sort-list">
+      <div id="card-list" className="sort-nav">
 
-          <div className="sort-list-item">
-            времени приготовления
+        <div>Sort by:</div>
+
+        <div className="sort-list-item">
+            Cooking time
             <div className="sort-item">
-              <div><a href={`${baseUrl}/?order=time&sort=ASC`}>по возрастанию</a></div>
-              <div><a href={`${baseUrl}/?order=time&sort=DESC`}>по убыванию</a></div>
+              <div><a href={`${baseUrl}/?order=time&sort=ASC`}><img width="20px" height="20px" src="/img/arrow_up.svg"/></a></div>
+              <div><a href={`${baseUrl}/?order=time&sort=DESC`}><img width="20px" height="20px" src="/img/arrow_down.svg"/></a></div>
             </div>
-          </div>
-  
-          <div className="sort-list-item">
-            ингредиентам
-            <div className="sort-item">
-              <div><a href={`${baseUrl}/?order=ingredientsCount&sort=ASC`}>по возрастанию</a></div>   
-              <div><a href={`${baseUrl}/?order=ingredientsCount&sort=DESC`}>по убыванию</a></div>
-            </div>
-          </div>
-  
         </div>
-    </div>
+        
+        <div className="sort-list-item">
+            ingredients
+            <div className="sort-item">
+              <div><a href={`${baseUrl}/?order=ingredientsCount&sort=ASC`}><img width="20px" height="20px" src="/img/arrow_up.svg"/></a></div>   
+              <div><a href={`${baseUrl}/?order=ingredientsCount&sort=DESC`}><img width="20px" height="20px" src="/img/arrow_down.svg"/></a></div>
+            </div>
+        </div>
 
-      <ul>
+      </div>
+
+      <ul className="card-group">
         {cards.map((card) => (
           <Card key={card.id} isAuth={isAuth} card={card} />
         ))}
